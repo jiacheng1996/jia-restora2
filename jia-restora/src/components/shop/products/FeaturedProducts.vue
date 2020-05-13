4c7acfe4d87bb4750146513cbf11534bafd7c046
jiacheng1996 <53596227+jiacheng1996@users.noreply.github.com>
2020-05-13 10:48:16 +0800

    Update FeaturedProducts.vue

--- jia-restora/src/components/shop/products/FeaturedProducts.vue
+++ jia-restora/src/components/shop/products/FeaturedProducts.vue
@@ -5,7 +5,76 @@
         <v-flex xs12>
           <h2 class="text-xs-center feature_products_title">Check Our <span>Delicious Menu</span></h2>
         </v-flex>
-         
+        <v-flex xs12>
+            <v-tabs>
+							<template v-for="item in items1">
+								<v-layout v-if="item.heading" :key="item.heading">
+									<v-flex xs6>
+										<v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+									</v-flex>
+									<v-flex xs6 class="text-xs-center">
+										<a href="#!" class="body-2 black--text">EDIT</a>
+									</v-flex>
+								</v-layout>
+								<v-list-group
+									v-else-if="item.children"
+									v-model="item.model"
+									:key="item.text"
+									:prepend-icon="item.model ? item.icon : item['icon-alt']"
+									append-icon
+									@click="pageLoad"
+								>
+									<v-list-tile slot="activator">
+										<v-list-tile-content>
+											<v-list-tile-title>{{ item.text }}</v-list-tile-title>
+										</v-list-tile-content>
+									</v-list-tile>
+									<v-list-tile v-for="(child, i) in item.children" :key="i">
+										<v-list-tile-action v-if="child.icon">
+											<v-icon>{{ child.icon }}</v-icon>
+										</v-list-tile-action>
+										<v-list-tile-content>
+											<v-list-tile-title>
+												<router-link
+													:to="{ name: child.link, params: {} }"
+													style="color:rgba(0,0,0,.87)"
+												>{{ child.text }}</router-link>
+											</v-list-tile-title>
+										</v-list-tile-content>
+									</v-list-tile>
+								</v-list-group>
+								<v-list-tile v-else :key="item.category">
+                      <v-tab v-for="item in items1" :key="item.text">
+                        {{ item.text }}
+                      </v-tab>
+                  <v-tab-items>
+									<v-tab-item v-for="(product,index) in products" :key="index">
+                    <v-card class="overlay_container flex_wrap  pa-2">
+                      <v-img :src="product.image" contain></v-img>
+                      <div style="width:100%;" class="flex_bottom text-xs-center pb-2">
+                        <h3 class="headline text-xs-center grey--text text--darken-3">{{product.item_name}}</h3>
+                        <h4 class="grey--text text--darken-3">{{currency}}{{product.price}}</h4>                                           
+                      </div>
+                      
+                      <v-card class="overlay">                       
+                          <h2 style="vertical-align:middle;">{{product.item_name}}</h2>                        
+                          <v-list class="details">
+                          <v-list-tile-action>
+                              <v-btn style="width:100%"  :to="'/product/' + product.id">Details</v-btn>
+                          </v-list-tile-action>
+                          <v-list-tile-action>
+                              <v-btn style="width:100%" class="main_color white--text" @click="addToCart(product)">Add To Cart</v-btn>
+                          </v-list-tile-action>
+                          </v-list>
+                      </v-card>   
+                    </v-card>                 
+									</v-tab-item>
+                  </v-tab-items>
+								</v-list-tile>
+								
+							</template>
+						</v-tabs>
+        </v-flex>
         <v-flex xs12 sm3 md3 lg3 xl2  class="flex_item" v-for="(product,index) in products" :key="index">
           <v-card class="overlay_container flex_wrap  pa-2">
             <v-img :src="product.image" contain></v-img>
@@ -35,13 +104,20 @@
 <script>
   import firebase from "firebase";
   import moment from 'moment' 
+  import MenuNavbar from  '@/components/shop/navbar/MenuNavbar'
   export default {
     data(){
+      
       return{
+        text:null,
         currency:null,
         products:[],
-        cart:this.$store.getters.cart 
+        cart:this.$store.getters.cart,
+        items1:[],
       }
+    },
+    components: {
+      MenuNavbar
     },
     methods: {
       productInCart(product) {
@@ -108,6 +184,23 @@
           }
         })
       })
+      // Show All Categories
+      let ref = db.collection("item_categories");
+      ref.onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type == "added") {
+            let doc = change.doc;
+            this.items1.push({
+            
+              icon: doc.data().category_icon,            
+              text: doc.data().category_name,
+              link:'CatProduct',
+              category:doc.data().category_name
+
+            });
+          }
+        });
+      });
     } 
   }
 </script>
